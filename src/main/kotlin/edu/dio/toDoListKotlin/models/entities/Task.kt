57d4af743ccaf34d6d75be9dcb573b107c4e9ec9
Bete.mrt <bete.mrt@gmail.com>
2023-10-04 01:08:43 -0300

    :construction: raw: entities

---
+++ src/main/kotlin/edu/dio/toDoListKotlin/models/entities/Task.kt
@@ -0,0 +1,32 @@
+package edu.dio.toDoListKotlin.models.entities
+
+import jakarta.persistence.*
+import java.time.LocalDate
+import java.time.LocalDate.now
+
+@Entity
+@Table(name = "tasks")
+data class Task (
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long? = null,
+
+    @Column(nullable = false)
+    var date: LocalDate = now(),
+
+    var title: String? = null,
+
+    @Column(nullable = false)
+    var description: String,
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    var status: StatusEnum = StatusEnum.TODO,
+
+    @Column(nullable = false)
+    var userId: Long
+) {
+    enum class StatusEnum {
+        TODO, IN_PROGRESS, COMPLETED
+    }
+}
---
+++ src/main/kotlin/edu/dio/toDoListKotlin/models/entities/User.kt
@@ -0,0 +1,28 @@
+package edu.dio.toDoListKotlin.models.entities
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "users")
+data class User (
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long? = null,
+
+    @Column(nullable = false, unique = true)
+    var username: String = "",
+
+    @Column(nullable = false, unique = true)
+    var email: String = "",
+
+    @Column(nullable = false)
+    var password: String = "",
+
+    @Column(nullable = true)
+    var imageUrl: String? = ""
+)
